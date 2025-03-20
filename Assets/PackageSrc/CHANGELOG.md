fa3848fec7df34c078341aa38f8e05c5dacf98c1
Surov Vladimir <vovkasu@gmail.com>
2025-03-20 18:00:13 +0200

    Added class ListExtendedMethods

---
+++ Assets/PackageSrc/CHANGELOG.md
@@ -0,0 +1,9 @@
+Changelog
+=========
+
+[1.0.0] - 2025.03.20
+--------------------
+* **Added**
+	* **ListExtendedMethods** - extends the capabilities of the List class
+* **Changed** -
+* **Fixed** -
---
+++ Assets/PackageSrc/Runtime/ListExtendedMethods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UAppToolKit
+{
+    public static class ListExtendedMethods
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        public static T GetRandomItem<T>(this IList<T> list)
+        {
+            return list[UnityEngine.Random.Range(0, list.Count)];
+        }
+
+        public static T GetRandomItemByWeight<T>(this IList<T> list, Func<T, int> heightGetter)
+        {
+            int allWeights = 0;
+            var weighed = new Dictionary<int, T>();
+            
+            foreach (var item in list)
+            {
+                allWeights += heightGetter(item);
+                weighed.Add(allWeights, item);
+            }
+            var range = UnityEngine.Random.Range(0, allWeights + 1);
+            foreach (var weighedKey in weighed.Keys)
+            {
+                if (range <= weighedKey)
+                {
+                    return weighed[weighedKey];
+                }
+            }
+
+            return weighed.Last().Value;
+        }
+    }
+}
